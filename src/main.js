bf277acec404a6a4f91d6080b43773e8df16c5e3
chip-student <omoram28@gmail.com>
2021-02-18 03:11:17 +0000

    Random Card Dealer version 1.1

--- src/main.js
+++ src/main.js
@@ -6,10 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
-
   // se declaran los arreglos
-  let simbolos = ["♠", "♥", "♦", "♣"];
+  let simbolos = ["card spade", "card diamond", "card club", "card heart"];
   let valores = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
   // se generan los random
@@ -17,12 +15,17 @@ window.onload = function() {
   let val = valores[Math.floor(Math.random() * valores.length)];
 
   // mostramos el resultado con los id asociados
+  //asignamos la clase
+  document.querySelector(".container").children[0].className = simb;
 
-  if (simb == "♥" || simb == "♦") {
-    document.getElementById("spa-before").style.color = "red";
-    document.getElementById("spa-after").style.color = "red";
+  let p = document.createElement("P");
+  p.innerHTML = val.toString();
+  p.style.fontSize = "50px";
+  p.style.fontfamily = "Arial, sans-serif";
+
+  if (simb == "card heart" || simb == "card diamond") {
+    p.style.color = "red";
   }
-  document.getElementById("spa-before").innerHTML = simb;
-  document.getElementById("textChange").innerHTML = val;
-  document.getElementById("spa-after").innerHTML = simb;
+  //Se ingresan el random del valores
+  document.querySelector(".container").children[0].append(p);
 };
